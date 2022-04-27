ffe7e5968193bfba7dfdbc674eaf564dd892289e
johnhunter-24 <johnjasperhunter@gmail.com>
2022-04-26 22:22:06 -0400

    34 to 40

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
+// Use
+// component name must be uppercase (UseStateBasics)
+// must be in the function/component body (const [text, setText] = useState('hello john');)
+// cannot call conditionally
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText] = useState('hello john');
+  const handleClick = () => {
+    if (text === 'hello john') {
+      setText('welcome to your app');
+    } else {
+      setText('hello john');
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        say hello
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
